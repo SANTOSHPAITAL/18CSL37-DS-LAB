42546e2c0c4bf0f78d172c5234fe5f25249d4836
Santosh Kumar Paital@21 <74662207+SANTOSHPAITAL@users.noreply.github.com>
2022-04-15 15:21:42 +0530

    Update and rename p8.1.c to 9.1-Poly.c

--- 9.1-Poly.c
+++ 9.1-Poly.c
@@ -1,3 +1,10 @@
+Design, Develop and Implement a Program in C for the following operations on Singly
+Circular Linked List (SCLL) with header nodes
+a. Represent and Evaluate a Polynomial P(x,y,z) = 6x2y
+2z-4yz5+3x3yz+2xy5z-2xyz3
+b. Find the sum of two polynomials POLY1(x,y,z) and POLY2(x,y,z) and store the result in
+POLYSUM(x,y,z)
+Support the program with appropriate functions for each of the above operations
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
